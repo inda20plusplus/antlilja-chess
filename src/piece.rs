3ca751f23d0b331760a305d5ec6676cd922666ee
antlilja <liljaanton2001@gmail.com>
2020-09-07 23:50:56 +0200

    Improved tests for Piece

--- src/piece.rs
+++ src/piece.rs
@@ -37,19 +37,26 @@ mod tests {
 
     #[test]
     fn opposite_piece_black_to_white() {
-        let piece = Piece::BlackBishop;
-        assert_eq!(piece.opposite(), Piece::WhiteBishop);
+        assert_eq!(Piece::BlackPawn.opposite(), Piece::WhitePawn);
+        assert_eq!(Piece::BlackRook.opposite(), Piece::WhiteRook);
+        assert_eq!(Piece::BlackKnight.opposite(), Piece::WhiteKnight);
+        assert_eq!(Piece::BlackBishop.opposite(), Piece::WhiteBishop);
+        assert_eq!(Piece::BlackQueen.opposite(), Piece::WhiteQueen);
+        assert_eq!(Piece::BlackKing.opposite(), Piece::WhiteKing);
     }
 
     #[test]
     fn opposite_piece_white_to_black() {
-        let piece = Piece::WhiteBishop;
-        assert_eq!(piece.opposite(), Piece::BlackBishop);
+        assert_eq!(Piece::WhitePawn.opposite(), Piece::BlackPawn);
+        assert_eq!(Piece::WhiteRook.opposite(), Piece::BlackRook);
+        assert_eq!(Piece::WhiteKnight.opposite(), Piece::BlackKnight);
+        assert_eq!(Piece::WhiteBishop.opposite(), Piece::BlackBishop);
+        assert_eq!(Piece::WhiteQueen.opposite(), Piece::BlackQueen);
+        assert_eq!(Piece::WhiteKing.opposite(), Piece::BlackKing);
     }
 
     #[test]
     fn opposite_piece_none() {
-        let piece = Piece::None;
-        assert_eq!(piece.opposite(), Piece::None);
+        assert_eq!(Piece::None.opposite(), Piece::None);
     }
 }
