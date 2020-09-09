14babdebacfdaddf07cceaf984d76f3fcef5f081
antlilja <liljaanton2001@gmail.com>
2020-09-09 20:47:26 +0200

    Added debug printing for Move struct

--- src/move.rs
+++ src/move.rs
@@ -1,4 +1,4 @@
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum MoveType {
     None,
     Pawn,
@@ -41,6 +41,12 @@ impl Move {
 
     pub fn is_castling(&self) -> bool {
         return (self.r#type as u8) > (MoveType::King as u8);
+    }
+}
+
+impl std::fmt::Debug for Move {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return write!(f, "[{:?}: {:?}]", self.r#type, self.to_xy());
     }
 }
 
