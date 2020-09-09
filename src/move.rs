47b6092bba074b105f42c05d286a2e821a0dfb17
antlilja <liljaanton2001@gmail.com>
2020-09-09 22:13:37 +0200

    Added pawn promotion move

--- src/move.rs
+++ src/move.rs
@@ -4,6 +4,7 @@ use crate::pos::Pos;
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Move {
     Move(PieceType, Pos),
+    PawnPromotion(PieceType, Pos),
     KingSideCastling,
     QueenSideCastling,
 }
