f9d0e0fc182dbbd40199fed0b731a34da3257be0
antlilja <liljaanton2001@gmail.com>
2020-09-10 19:48:33 +0200

    Added test for pawn moves

--- src/board_tests.rs
+++ src/board_tests.rs
@@ -5,14 +5,14 @@ fn new_board() {
     let board = Board::new();
 
     let exists_at_both_sides = |offset, r#type| {
-    assert_eq!(
-        board.at_index(offset),
-        ColoredPiece::new(r#type, Color::White)
-    );
-    assert_eq!(
-        board.at_index(8 * 7 + offset),
-        ColoredPiece::new(r#type, Color::Black)
-    );
+        assert_eq!(
+            board.at_index(offset),
+            ColoredPiece::new(r#type, Color::White)
+        );
+        assert_eq!(
+            board.at_index(8 * 7 + offset),
+            ColoredPiece::new(r#type, Color::Black)
+        );
     };
 
     let exists_matching_at_both_sides = |offset, r#type| {
@@ -40,4 +40,38 @@ fn new_board() {
     exists_matching_at_both_sides(2, PieceType::Bishop);
     exists_at_both_sides(3, PieceType::Queen);
     exists_at_both_sides(4, PieceType::King);
+}
+
+#[test]
+fn pawn_moves_new_board() {
+    let board = Board::new();
+
+    let mut moves = Vec::<Move>::with_capacity(10);
+    for x in 0..8 {
+        // Top of board
+        let mut check_side = |y_start, y_dir: i8| {
+            let move_to = |to_x, to_y| {
+                let from = Pos::from_xy(x, y_start);
+                let to = Pos::from_xy(to_x, to_y);
+                Move::Move(from, to)
+            };
+
+            let correct_moves = [
+                move_to(x, (y_start as i8 + y_dir) as u8),
+                move_to(x, (y_start as i8 + y_dir * 2) as u8),
+            ];
+
+            let count = board.get_moves_for(&mut moves, x, y_start);
+            assert_eq!(count, correct_moves.len());
+
+            for (i, m) in moves.iter().enumerate() {
+                assert_eq!(m, &correct_moves[i]);
+            }
+
+            moves.clear();
+        };
+
+        check_side(1, 1);
+        check_side(6, -1);
+    }
 }
