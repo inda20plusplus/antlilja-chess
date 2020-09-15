2da0f7152f81c72588443eb1644f1294ee450def
antlilja <liljaanton2001@gmail.com>
2020-09-15 11:25:27 +0200

    Added super simple CLI interface

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,8 +2,70 @@ extern crate chess;
 
 use chess::*;
 
-fn main() {
-    let game = Game::new();
+fn parse_pos(str: &str) -> (u8, u8) {
+    let mut chars = str.chars();
+    return (
+        chars.next().unwrap() as u8 - 97,
+        chars.next().unwrap() as u8 - 49,
+    );
+}
 
-    game.print_ascii();
+fn parse_move(str: &String) -> Option<((u8, u8), (u8, u8))> {
+    let mut positions = str.split_whitespace();
+
+    let from = positions.next();
+    let to = positions.next();
+    if from.is_none() || to.is_none() || from.unwrap().len() != 2 || to.unwrap().len() != 2 {
+        return None;
+    }
+
+    return Some((parse_pos(from.unwrap()), parse_pos(to.unwrap())));
+}
+
+fn main() {
+    let mut game = Game::new();
+
+    let mut buffer = String::new();
+
+    loop {
+        buffer.clear();
+        game.print_ascii();
+        println!("Make your move: ");
+        std::io::stdin().read_line(&mut buffer).unwrap();
+        let r#move = parse_move(&buffer);
+
+        if r#move.is_none() {
+            println!("Not a valid move: {}", buffer);
+            continue;
+        }
+
+        let r#move = r#move.unwrap();
+        let from = r#move.0;
+        let to = r#move.1;
+
+        let moves = game.get_moves_for(from.0, from.1);
+
+        if moves.is_none() {
+            println!("Not a valid move: {}", buffer);
+            continue;
+        }
+
+        let r#move = Move::Move(Pos::from_xy(to.0, to.1));
+
+        let moves = moves.unwrap();
+
+        let move_index = moves.find(r#move);
+
+        if move_index.is_none() {
+            println!("Not a valid move: {}", buffer);
+            continue;
+        }
+
+        let move_index = move_index.unwrap();
+
+        if game.play(from.0, from.1, move_index) == Result::Checkmate {
+            println!("{:?} lost :(", game.current_color());
+            break;
+        }
+    }
 }
