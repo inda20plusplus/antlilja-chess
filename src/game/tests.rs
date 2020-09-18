58cc5ffc651a30cc5b5b9e53a684ebae79382d7b
antlilja <liljaanton2001@gmail.com>
2020-09-18 19:37:17 +0200

    Added a test for Game that plays a whole game of chess

--- src/game/tests.rs
+++ src/game/tests.rs
@@ -83,3 +83,30 @@ fn king_moves_new_board() {
     game.switch_side();
     assert!(game.get_moves_for(4, 7).unwrap().is_empty());
 }
+
+#[test]
+fn test_with_whole_game() {
+    let mut game = Game::new();
+    let moves = "
+            e4 e6 d4 d5 Nd2 Nf6 e5 Nfd7 f4 c5 c3 Nc6 Ndf3 cxd4 cxd4 f6 Bd3 Bb4+ Bd2 
+            Qb6 Ne2 fxe5 fxe5 O-O a3 Be7 Qc2 Rxf3 gxf3 Nxd4 Nxd4 Qxd4 O-O-O Nxe5 Bxh7+ 
+            Kh8 Kb1 Qh4 Bc3 Bf6 f4 Nc4 Bxf6 Qxf6 Bd3 b5 Qe2 Bd7 Rhg1 Be8 Rde1 Bf7 Rg3 
+            Rc8 Reg1 Nd6 Rxg7 Nf5 R7g5 Rc7 Bxf5 exf5 Rh5+ Bxh5 Qxh5+ Qh6 Qxh6+ Rh7 Qf8#";
+
+    let moves = moves.trim().split_whitespace();
+
+    for (i, str_move) in moves.enumerate() {
+        let ((x, y), actual_move) = game.parse_pgn_move(str_move);
+        if actual_move == Move::None {
+            println!("{}", str_move);
+        }
+        assert_ne!(actual_move, Move::None);
+
+        let result = game.play(x, y, actual_move);
+        if result == Result::Checkmate {
+            assert_eq!(game.current_color(), Color::Black);
+            assert_eq!(i, 68);
+            break;
+        }
+    }
+}
