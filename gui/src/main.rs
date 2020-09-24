6211c2b9e7d493d6cef53d8022f4b6d8a78dc704
Markus Videfors <markus.videfors@outlook.com>
2020-09-25 01:38:53 +0200

    use controller and view in event loop

---
+++ gui/src/main.rs
@@ -0,0 +1,27 @@
+extern crate chess;
+extern crate gui;
+
+use chess::game::{Game, GameResult};
+use gui::game_controller::GameController;
+use gui::view::{View, ViewSettings};
+use piston_window::{PistonWindow, WindowSettings};
+
+
+fn main() {
+    let mut game = Game::default();
+
+    let mut controller = GameController::new(game);
+    let mut view = View::new(ViewSettings::default());
+
+    let mut window: PistonWindow = WindowSettings::new("Chess", [1024, 640])
+        .build()
+        .unwrap();
+
+    while let Some(event) = window.next() {
+        controller.event(&event);
+
+        window.draw_2d(&event, |context, graphics, _| {
+            view.render(&controller, context, graphics);
+        });
+    };
+}
