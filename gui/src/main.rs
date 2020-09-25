0a816de03d51488f3872f52adfe349afb95c80f9
Markus Videfors <markus.videfors@outlook.com>
2020-09-25 03:30:31 +0200

    create View with textures

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -11,11 +11,13 @@ fn main() {
     let mut game = Game::default();
 
     let mut controller = GameController::new(game);
-    let mut view = View::new(ViewSettings::default());
 
     let mut window: PistonWindow = WindowSettings::new("Chess", [1024, 640])
         .build()
         .unwrap();
+
+    let textures = View::create_textures(&mut window);
+    let mut view = View::new(ViewSettings::default(), textures);
 
     while let Some(event) = window.next() {
         controller.event(&event);
