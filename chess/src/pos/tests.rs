0fc14cda09606a393dc93dee81a8a78511a44848
antlilja <liljaanton2001@gmail.com>
2020-09-27 17:16:08 +0200

    Added new tests to Pos struct Also added new macro: assert_panic

--- chess/src/pos/tests.rs
+++ chess/src/pos/tests.rs
@@ -1,5 +1,16 @@
 use super::*;
 
+macro_rules! assert_panic {
+    ($expression:expr) => {
+        if !std::panic::catch_unwind(|| $expression).is_err() {
+            panic!(
+                "Assertion didn't panic on expression: {}",
+                stringify!($expression)
+            );
+        }
+    };
+}
+
 #[test]
 fn equal_to_index() {
     for x in 0..8 {
@@ -9,6 +20,28 @@ fn equal_to_index() {
 
             assert_eq!(index, pos_index);
         }
+    }
+}
+
+#[test]
+fn new_xy_invalid() {
+    for x in 8..=255 {
+        for y in 0..8 {
+            assert_panic!(Pos::new_xy(x, y));
+        }
+    }
+
+    for x in 0..8 {
+        for y in 8..=255 {
+            assert_panic!(Pos::new_xy(x, y));
+        }
+    }
+}
+
+#[test]
+fn new_index_invalid() {
+    for i in 64..=255 {
+        assert_panic!(Pos::new_index(i));
     }
 }
 
@@ -166,6 +199,26 @@ fn move_xy() {
                         assert_eq!(new_pos.x(), (x as i8 + x2) as u8);
                         assert_eq!(new_pos.y(), (y as i8 + y2) as u8);
                     }
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn distance() {
+    for x in 0..8 {
+        for y in 0..8 {
+            let pos = Pos::new_xy(x, y);
+            for x2 in 0..8 {
+                for y2 in 0..8 {
+                    let pos2 = Pos::new_xy(x2, y2);
+                    let dist_x = pos.distance_x(&pos2);
+                    let dist_y = pos.distance_y(&pos2);
+                    let rdist_x = (x as i8 - x2 as i8).abs() as u8;
+                    let rdist_y = (y as i8 - y2 as i8).abs() as u8;
+                    assert_eq!(dist_x, rdist_x);
+                    assert_eq!(dist_y, rdist_y);
                 }
             }
         }
