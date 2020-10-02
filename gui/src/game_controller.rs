a983e14a01336cfe9b362c9c62193d993107dc2f
Markus Videfors <markus.videfors@outlook.com>
2020-10-02 10:29:54 +0200

    refactor code into multiple methods

--- gui/src/game_controller.rs
+++ gui/src/game_controller.rs
@@ -1,6 +1,7 @@
 use chess::game::Game;
 use chess::Move;
 use chess::Pos;
+use crate::view::ViewSettings;
 use piston_window::{Button, GenericEvent, MouseButton};
 use std::collections::HashMap;
 
@@ -19,16 +20,18 @@ pub enum State {
 pub struct GameController {
     pub game: Game,
     pub state: State,
+    settings: ViewSettings,
     pub selected_square: Option<[usize; 2]>,
     pub current_moves: Option<HashMap<[usize; 2], Move>>,
     cursor_pos: [f64; 2],
 }
 
 impl GameController {
-    pub fn new(game: Game) -> GameController {
+    pub fn new(game: Game, settings: ViewSettings) -> GameController {
         GameController {
             game,
             state: State::Playing,
+            settings,
             selected_square: None,
             current_moves: None,
             cursor_pos: [0.0; 2],
@@ -41,58 +44,88 @@ impl GameController {
         }
 
         if let Some(Button::Mouse(MouseButton::Left)) = e.press_args() {
-            let x = self.cursor_pos[0] - board_pos[0] - 2.0;
-            let y = self.cursor_pos[1] - board_pos[1] - 2.0;
-
-            if x >= 0.0 && x < board_size && y >= 0.0 && y < board_size {
-                let cell_x = (x / board_size * 8.0) as usize;
-                let cell_y = 7 - (y / board_size * 8.0) as usize;
-
-                if let Some(moves) = &self.current_moves {
-                    if let Some(r#move) = moves.get(&[cell_x, cell_y]) {
-                        let from = self.selected_square.unwrap();
-                        self.game.play_xy(from[0] as u8, from[1] as u8, *r#move);
-
-                        self.selected_square = None;
-                        self.current_moves = None;
-                        return;
-                    }
-                }
-                self.selected_square = Some([cell_x, cell_y]);
-
-                self.current_moves = match self
-                    .game
-                    .moves_for_pos(Pos::new_xy(cell_x as u8, cell_y as u8))
-                {
-                    Some(move_slice) => {
-                        let mut moves = HashMap::new();
-                        println!("debug:");
-                        for r#move in move_slice.iter() {
-                            println!("{:?}", r#move);
-                            match r#move {
-                                Move::Move(pos) => {
-                                    moves.insert([pos.x() as usize, pos.y() as usize], *r#move);
-                                }
-                                Move::EnPassant(pos) => {
-                                    moves.insert([pos.x() as usize, pos.y() as usize], *r#move);
-                                }
-                                Move::PawnPromotion(_, pos) => {
-                                    moves.insert([pos.x() as usize, pos.y() as usize], *r#move);
-                                }
-                                Move::KingSideCastling => {
-                                    moves.insert([cell_x + 2, cell_y], *r#move);
-                                }
-                                Move::QueenSideCastling => {
-                                    moves.insert([cell_x - 3, cell_y], *r#move);
-                                }
-                                _ => (),
-                            }
-                        }
-                        Some(moves)
-                    }
-                    None => None,
-                };
+            match self.state {
+                State::Playing => self.play_input(),
+                _ => (),
             }
+        }
+    }
+
+    fn play_input(&mut self) {
+        let board_size = self.settings.board_size;
+        let board_pos = [self.settings.padding, 0.0];
+
+        let x = self.cursor_pos[0] - board_pos[0] - 2.0;
+        let y = self.cursor_pos[1] - board_pos[1] - 2.0;
+
+        if x >= 0.0 && x < board_size && y >= 0.0 && y < board_size {
+            let cell_x = (x / board_size * 8.0) as usize;
+            let cell_y = 7 - (y / board_size * 8.0) as usize;
+
+            if let Some(moves) = &self.current_moves {
+                if let Some(r#move) = moves.get(&[cell_x, cell_y]) {
+
+                    let from = self.selected_square.unwrap();
+                    let turn_result = self.game.play_xy(from[0] as u8, from[1] as u8, *r#move);
+
+                    match turn_result {
+                        GameResult::Ok => (),
+                        GameResult::Checkmate => {
+                            let color = match self.game.current_color() {
+                                Color::White => Ending::White,
+                                Color::Black => Ending::Black,
+                            };
+                            self.state = State::End(color);
+                        }
+                        GameResult::Stalemate => self.state = State::End(Ending::Tie),
+                        GameResult::InvalidMove => panic!("Move was in current move but game returned InvalidMove")
+                    }
+
+                    self.selected_square = None;
+                    self.current_moves = None;
+                } else {
+                    self.selected_square = Some([cell_x, cell_y]);
+                    self.get_current_moves();
+                }
+            } else {
+                self.selected_square = Some([cell_x, cell_y]);
+                self.get_current_moves();
+            }
+        }
+    }
+
+    fn get_current_moves(&mut self) {
+        if let Some(position) = self.selected_square {
+            let current_pos = Pos::new_xy(position[0] as u8, position[1] as u8);
+            self.current_moves = match self.game.moves_for_pos(current_pos) {
+                Some(move_slice) => {
+                    let mut moves = HashMap::new();
+                    println!("debug:");
+                    for r#move in move_slice.iter() {
+                        println!("{:?}", r#move);
+                        match r#move {
+                            Move::Move(pos) => {
+                                moves.insert([pos.x() as usize, pos.y() as usize], *r#move);
+                            }
+                            Move::EnPassant(pos) => {
+                                moves.insert([pos.x() as usize, pos.y() as usize], *r#move);
+                            }
+                            Move::PawnPromotion(_, pos) => {
+                                moves.entry([pos.x() as usize, pos.y() as usize]).or_insert(*r#move);
+                            }
+                            Move::KingSideCastling => {
+                                moves.insert([position[0] + 2, position[1]], *r#move);
+                            }
+                            Move::QueenSideCastling => {
+                                moves.insert([position[0] - 3, position[1]], *r#move);
+                            }
+                            _ => (),
+                        }
+                    }
+                    Some(moves)
+                }
+                None => None,
+            };
         }
     }
 }
