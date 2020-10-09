addc2b13695646c6508311a305734969b9a8cdb2
Markus Videfors <markus.videfors@outlook.com>
2020-10-09 11:19:18 +0200

    make field public

--- gui/src/network.rs
+++ gui/src/network.rs
@@ -123,7 +123,7 @@ impl Message {
 }
 
 pub struct ConnectionHandler {
-    is_host: bool,
+    pub is_host: bool,
     stream: Arc<Mutex<TcpStream>>,
     recieved_messages: Arc<Mutex<VecDeque<[u8; 5]>>>,
     read_handle: Option<thread::JoinHandle<()>>,
