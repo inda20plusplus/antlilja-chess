34dd6fab637702da0ec06e889c9b4773fb55f16a
Markus Videfors <markus.videfors@outlook.com>
2020-10-09 11:10:25 +0200

    create method to spawn a continuous reader

--- gui/src/network.rs
+++ gui/src/network.rs
@@ -6,3 +6,28 @@ pub struct ConnectionHandler {
     recieved_messages: Arc<Mutex<VecDeque<[u8; 5]>>>,
     read_handle: Option<thread::JoinHandle<()>>,
 }
+
+impl ConnectionHandler {
+    fn spawn_read_thread(&mut self) -> thread::JoinHandle<()> {
+        let stream = Arc::clone(&self.stream);
+        let queue = Arc::clone(&self.recieved_messages);
+
+        thread::spawn(move || loop {
+            let mut buf = [0; 5];
+
+            let mut stream = stream.lock().unwrap();
+            let mut queue = queue.lock().unwrap();
+
+            let result = stream.read(&mut buf).unwrap();
+
+            if result == 0 {
+                break;
+            };
+
+            queue.push_front(buf);
+
+            drop(stream);
+            drop(queue);
+        })
+    }
+}
