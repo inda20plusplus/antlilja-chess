1f70bf22e53a7060ad2f3e4bf6eeb9512ba494fd
Markus Videfors <markus.videfors@outlook.com>
2020-10-09 11:09:05 +0200

    add field to store thread handle

--- gui/src/network.rs
+++ gui/src/network.rs
@@ -4,4 +4,5 @@ pub struct ConnectionHandler {
     is_host: bool,
     stream: Arc<Mutex<TcpStream>>,
     recieved_messages: Arc<Mutex<VecDeque<[u8; 5]>>>,
+    read_handle: Option<thread::JoinHandle<()>>,
 }
