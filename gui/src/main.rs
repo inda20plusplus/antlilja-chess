942255c77dea0c250cd0c5a9459ee0ed375fd20c
Markus Videfors <markus.videfors@outlook.com>
2020-10-11 17:54:55 +0200

    use read_line instead of read_string

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -10,7 +10,7 @@ fn get_input() -> String {
     let mut input = String::new();
 
     io::stdin()
-        .read_to_string(&mut input)
+        .read_line(&mut input)
         .expect("Failed to read input");
 
     input.trim().to_string()
